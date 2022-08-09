42ecc38d91dbed60f1119bb42359b2ea194609f0
fernandofreitas03 <fernandofreitas130gmail.com>
2022-08-09 16:41:16 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,72 @@
+const {request, response } = require('express')
+const express = require('express')
+const uuid = require('uuid')
+const cors = require('cors')
+const port = 3001
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+const users = []
+
+const checkUserId = (request, response, next) => {
+    const { id } = request.params
+    
+    const index = users.findIndex(user => user.id === id)
+   
+    if(index < 0){
+        return response.status(404).json({message: 'User not found'})
+    }
+    
+    request.userIndex = index
+    request.userId = id
+
+   
+    next()
+}
+
+
+app.get('/users', (request, response) => {
+   return response.json(users)
+})
+
+app.post('/users', (request, response) => {
+   const {name, age} = request.body
+  
+   const user = { id: uuid.v4(), name, age} 
+ 
+   users.push(user)
+  
+   return response.status(201).json(user)
+})
+
+app.put('/users/:id', checkUserId,(request, response) => {
+    
+    const {name, age} = request.body
+    const index = request.userIndex
+    const id = request.userId
+
+    const updateUsers = {name, age, id}
+
+    users[index] = updateUsers
+
+    return response.json(updateUsers)
+})
+
+app.delete('/users/:id', checkUserId, (request, response) => {
+   const index = request.userIndex
+
+    users.splice(index, 1)
+
+
+    return response.status(204).json()
+})
+
+
+
+
+
+app.listen(port, () =>{
+    console.log(`Served starde ${port} 🚀`)
+})
